44bcf929f7e2265b887ea5a00e17d5b877f8eb83
Your Name <yh02195@naver.com>
2021-05-27 01:06:31 +0900

    junit er

--- board/src/main/java/board/configuration/DatabaseConfiguration.java
+++ board/src/main/java/board/configuration/DatabaseConfiguration.java
@@ -2,7 +2,12 @@ package board.configuration;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -13,6 +18,9 @@ import com.zaxxer.hikari.HikariDataSource;
 @Configuration
 @PropertySource("classpath:/application.properties") //application.properties를 사용할 수 있도록 설정 파일의 위치를 정해 준다.
 public class DatabaseConfiguration {
+	
+	@Autowired
+	private ApplicationContext applicationContext;
 
     // application.properties에 설정했던 데이터베이스 관련 정보를 사용하도록 지정한다.
 	//@ConfigurationProperties 어노테이션에 prefix가 spring.datasource.hikari로 설정되었기 때문에 
@@ -30,5 +38,18 @@ public class DatabaseConfiguration {
         DataSource dataSource = new HikariDataSource(hikariConfig());
         System.out.println(dataSource.toString());
         return dataSource;
+    }
+    
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        sqlSessionFactoryBean.setMapperLocations(applicationContext.getResources("classpath:/mapper/**/*.xml"));	//** : 하위폴더전체
+        return sqlSessionFactoryBean.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
     }
 }
