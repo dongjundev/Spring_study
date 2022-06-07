13a06cb7b2e8d2ae656fe8565822d107b0570b1f
dongjun <yh02195@gmail.com>
2022-06-07 23:02:45 +0900

    템플릿 콜백 패턴 적용

--- advanced/src/main/java/hello/advanced/app/v4/OrderControllerV4.java
+++ advanced/src/main/java/hello/advanced/app/v4/OrderControllerV4.java
@@ -4,11 +4,13 @@ import hello.advanced.trace.TraceStatus;
 import hello.advanced.trace.logtrace.LogTrace;
 import hello.advanced.trace.template.AbstractTemplate;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class OrderControllerV4 {
 
     private final OrderServiceV4 orderService;
@@ -24,6 +26,7 @@ public class OrderControllerV4 {
                 return "ok";
             }
         };
+        log.info(String.valueOf(trace.getClass()));
         return template.execute("OrderController.request()");
     }
 }
